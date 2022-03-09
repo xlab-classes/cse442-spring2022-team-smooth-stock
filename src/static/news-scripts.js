22fdef2ef4126dbfabb47fb430212262b961ac37
Jake Heidman <jake.heidman2@gmail.com>
2022-03-09 13:48:11 -0500

    remove dead button click event (#36)

--- src/static/news-scripts.js
+++ src/static/news-scripts.js
@@ -5,21 +5,4 @@ $(document).ready(function() {
     var newText = document.createTextNode('Meta');
     newCell.append(newText);
     let tickerCell = metaRow.insertCell().append(document.createTextNode('META'));
-
-
-    $('button').click(function(event) {
-        //let urlCall = "./run?query=" + escape(view_query)
-        $.get("./createaccount", function(){
-            $.ajax({
-                url: '/createaccount',
-                type: 'POST',
-                success: function(response){
-                    console.log(response);
-                },
-                error: function(error){
-                    console.log(error);
-                }
-            })
-        });
-    });
 })
