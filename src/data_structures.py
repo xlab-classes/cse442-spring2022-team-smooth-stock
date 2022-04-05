0c2bdc22857c341eb529dc876ddec6d84f2807c7
kevintodd36 <kevintodd36@gmail.com>
2022-04-05 19:58:16 -0400

    #70 Swapped over from sqlacademy to mysql
    Save data and get data from the mysql server at buffalo, also redo the entire login authentication system because I cant store an object in mysql

--- src/data_structures.py
+++ src/data_structures.py
@@ -7,3 +7,11 @@ class Account(UserMixin, app.database.Model):
     email = app.database.Column(app.database.String(40), unique=True)
     password = app.database.Column(app.database.String(50))
     salt = app.database.Column(app.database.String(100))
+
+class User(UserMixin):
+    is_authenticated = False
+    is_active = False
+    is_anonymous = False
+    id = 0
+    def get_id(self):
+        return (self.id)
