3b6cfb1f5fed485289278c0a3ef765403cd2550c
miclaszlo <mlaszlo409@gmail.com>
2022-04-08 16:11:50 -0400

    Moved function bodies to path_calls.py

--- src/app.py
+++ src/app.py
@@ -97,10 +97,9 @@ def return_notify_page():
    return render_template('notify.html')
 
 @app.route('/discover')
-@login_required
+#@login_required
 def return_discover_page():
    return render_template('discover.html')
-
 
 @app.route('/logout')
 @login_required
@@ -115,103 +114,14 @@ def create_tables():
 @app.route('/follow')
 #@login_required
 def follow():
-
-   # Connect to database
-   mydb = mysql.connector.connect(
-      host="oceanus.cse.buffalo.edu",
-      user="mdlaszlo",
-      password="50265202",
-      database="cse442_2022_spring_team_q_db"
-   )
-
-   # Create cursor
-   cursor = mydb.cursor()
-
-   # Disable Foreign Key Checks (for now)
-   sql = "SET FOREIGN_KEY_CHECKS=0"
-   cursor.execute(sql)
-
-   # Create sql command
-   sql = "INSERT INTO saved_stocks (userID, username, stocks) VALUES (%s, %s, %s)"
-
-   # Create values
-   vals = ("-1", "fake_user_1", "APPL, MSFT, TSLA")
-
-   # Execute command
-   cursor.execute(sql, vals)
-   mydb.commit()
-
-   # Print saved_stocks information
-   cursor.execute("SELECT * FROM saved_stocks")
-   myresult = cursor.fetchall()
-
-   for x in myresult :
-      print(x)
-
-   # Re-enable Foreign Key Checks (for now)
-   sql = "SET FOREIGN_KEY_CHECKS=1"
-   cursor.execute(sql)
-
-   # Render initial discover page (for now)
-   return render_template('discover.html')
+   return(path_calls.follow())
    
 
 @app.route('/find-stock', methods=["POST"])
 #@login_required
 def return_discover_template_page():
-
-   """
-   Variables to be obtained from Yahoo Finance API to be displayed on web page:
-   1. Price History
-   2. Fifty_Two_Week_Range
-   3. Fifty_Day_Average
-   4. Two_Hundred_Day_Average
-   5. EPS_Current_Year
-   6. Price_EPS_Current_Year
-   7. Average_Analyst_Rating
-   8. Stock_Name -> Symbol of the stock being searched
-   9. Company
-   10. Current_Stock_Price
-   11. Current_plus_minus
-   """
-
-   # Get the company sybmol user is looking for
-   company_symbol = request.form.get('stock')
-   print(company_symbol)
-
-   # Create url for Yahoo Finace API
-   url = "https://yfapi.net/v6/finance/quote"
-
-   # Create querystring to find company stock info
-   querystring = {"symbols":company_symbol}
+   return(path_calls.return_discover_template_page())
    
-   # Create headers dictionary with API Key
-   headers = {
-      'x-api-key': "REiSqBThOa9z6bIgDGJ2l4S92jMKXl8O1yRsROBK"
-   }
-
-   # Send request to Yahoo Finance API and store response
-   response = requests.request("GET", url, headers=headers, params=querystring)
-
-   # Load response as a dictionary
-   dict = json.loads(response.text)
-
-   # Initialize variables stated at beginning of function 
-   stock_symbol = dict.get('quoteResponse').get('result')[0].get('symbol')
-   company = dict.get('quoteResponse').get('result')[0].get('displayName')
-   current_stock_price = str(dict.get('quoteResponse').get('result')[0].get('regularMarketPrice'))
-   current_plus_minus = str(dict.get('quoteResponse').get('result')[0].get('regularMarketChangePercent'))
-   price_history = company + "'s Price History"
-   fifty_two_week_range = "52-Week Range: " + dict.get('quoteResponse').get('result')[0].get('fiftyTwoWeekRange')
-   fifty_day_average = "50 Day average: " + str(dict.get('quoteResponse').get('result')[0].get('fiftyDayAverage'))
-   two_hundred_day_average = " 200 Day Average: " + str(dict.get('quoteResponse').get('result')[0].get('twoHundredDayAverage'))
-   eps_current_year = " EPS Current Year: " + str(dict.get('quoteResponse').get('result')[0].get('epsCurrentYear'))
-   price_eps_current_year = " Price EPS Current Year: " + str(dict.get('quoteResponse').get('result')[0].get('priceEpsCurrentYear'))
-   average_analyst_rating = " Average Analyst Rating: " + dict.get('quoteResponse').get('result')[0].get('averageAnalystRating')
-
-   # Return html page to be rendered
-   return render_template('discover_template.html', Stock_Name=stock_symbol, Company=company, Current_Stock_Price=current_stock_price, Current_plus_minus=current_plus_minus, Price_History=price_history, Fifty_Two_Week_Range=fifty_two_week_range, Fifty_Day_Average=fifty_day_average, Two_Hundred_Day_Average=two_hundred_day_average, EPS_Current_Year=eps_current_year, Price_EPS_Current_Year=price_eps_current_year, Average_Analyst_Rating=average_analyst_rating)
-
 @app.route('/442')
 def return_442_page():
    time.sleep(3)
@@ -238,13 +148,7 @@ def return_support_page():
     return render_template('support.html', generate_table=table_head)
 
 
-
-
-
-
-
 # test
-
 @app.route('/test_login')
 @login_required
 def test_login():
