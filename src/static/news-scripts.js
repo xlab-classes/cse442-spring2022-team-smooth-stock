d0414a71e2d3ccccc5c1b3d6f65fc4758c687cc1
Jake Heidman <jake.heidman2@gmail.com>
2022-04-13 12:57:50 -0400

    #77 remove unused code ran at document load

--- src/static/news-scripts.js
+++ src/static/news-scripts.js
@@ -1,8 +1,2 @@
 $(document).ready(function() {
-    var newsTableBody = document.getElementById('news-table').getElementsByTagName('tbody')[0];    
-    var metaRow = newsTableBody.insertRow();
-    var newCell = metaRow.insertCell();
-    var newText = document.createTextNode('Meta');
-    newCell.append(newText);
-    let tickerCell = metaRow.insertCell().append(document.createTextNode('META'));
 })
