e82b113911f15c74ea0fe303be24952364aaaf36
Jake Heidman <jake.heidman2@gmail.com>
2022-04-27 13:55:02 -0400

    #105 update related news to have maximum width, add twitter to ticker conversion function

--- path_calls.py
+++ path_calls.py
@@ -399,12 +399,15 @@ def get_user_stocks(username):
         return []
 
 def ticker_to_stock_name(ticker):
-    if ticker == "GOOG":
+    ticker = ticker.lower()
+    if ticker == "goog":
         return "google"
-    elif ticker == "AAPL":
+    elif ticker == "aapl":
         return "apple"
-    elif ticker == "MSFT":
+    elif ticker == "msft":
         return "microsoft"
+    elif ticker == "twtr":
+        return "twitter" 
     else:
         return "not implemented"
 
